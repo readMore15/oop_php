a11dfda5f5f0d4de36335a260cc506600d693231
readMore15 <danemmfabrigar@gmail.com>
2015-08-20 13:52:33 +0800

    Updated files

--- view.Address.php
+++ view.Address.php
@@ -24,13 +24,39 @@
 		$address->birth_date = '25';
 		$address->birth_month = 'Oct';
 		$address->birth_year = '1988';
+		$address->person_status_id = 3;
 
-		/*echo '<pre>';
+		echo '<pre>';
 		echo var_export($address);
-		echo '</pre>';*/
+		echo '</pre>';
 
 		echo $address->display_details();
 
+	?>
+
+	<h2>Testing magic method __get and __set</h2>
+
+	<?php
+		unset($address->birth_year);
+		echo $address->display_details();
+	?>
+
+	<h2>Static word/Scope resolution</h2>
+
+	<?php
+		echo '<pre>';
+		echo var_export(Address::$person_status);
+		echo '</pre>';
+	?>
+
+	<h2>Testing static method</h2>
+
+	<?php
+		for ($i=0; $i <= 4; $i++) { 
+			echo '<div>' . $i . ' is ';
+			echo  Address::isPersonStatusValid($i) ? 'Valid' : 'Invalid';
+			echo "</div>";
+		}
 	?>
 
 </body>
